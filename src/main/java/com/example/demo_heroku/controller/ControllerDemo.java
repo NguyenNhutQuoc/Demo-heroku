a271332f7ca96690d025fd94825cd157ec7fd6e1
NguyenNhutQuoc <nguyennhutquoc1412@gmail.com>
2022-04-26 22:21:04 +0700

    better and better

---
+++ src/main/java/com/example/demo_heroku/controller/ControllerDemo.java
@@ -0,0 +1,16 @@
+package com.example.demo_heroku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class ControllerDemo {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
